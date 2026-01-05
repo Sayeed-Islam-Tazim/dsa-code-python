6fe8e4f94842f852db0910954290c028b76df3c8
Sayeed Islam Tazim <sayeeditt@gmail.com>
2026-01-06 01:57:37 +0600

    more dsa codes added

---
+++ 001-palindrome-number.py
@@ -0,0 +1,11 @@
+n = 43534
+num = n
+result = 0
+while num > 0:
+    ld = num % 10
+    result = result * 10 + ld
+    num = num // 10
+if result == n:
+    print("palindrome")
+else:
+    print("not palindrome")

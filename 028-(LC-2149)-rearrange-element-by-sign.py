e28c1aa9ee3c38f7e74cc06be770bc66e52d258d
Sayeed Islam Tazim <sayeeditt@gmail.com>
2026-01-14 23:33:31 +0600

    rearrange element by sign code added

--- 028-(LC-2149)-rearrange-element-by-sign.py
+++ 028-(LC-2149)-rearrange-element-by-sign.py
@@ -1,21 +1,21 @@
 def rearrange_element_by_sign(nums):
 
     # 1st Approach
-    for i in range(0, len(nums)):
-        j = i + 1
-        if nums[i] >= 0 and j < len(nums):
-            while nums[j] >= 0:
-                j += 1
-            nums[j], nums[i+1] = nums[i+1], nums[j]
-        else:
-            continue
-        if nums[i] < 0 and j < len(nums):
-            while nums[j] < 0:
-                j += 1
-            nums[j], nums[i+1] = nums[i+1], nums[j]
-        else:
-            continue 
-    return nums
+    # for i in range(0, len(nums)):
+    #     j = i + 1
+    #     if nums[i] >= 0 and j < len(nums):
+    #         while nums[j] >= 0:
+    #             j += 1
+    #         nums[j], nums[i+1] = nums[i+1], nums[j]
+    #     else:
+    #         continue
+    #     if nums[i] < 0 and j < len(nums):
+    #         while nums[j] < 0:
+    #             j += 1
+    #         nums[j], nums[i+1] = nums[i+1], nums[j]
+    #     else:
+    #         continue 
+    # return nums
     
     
     # 2nd Approach
@@ -32,17 +32,17 @@ def rearrange_element_by_sign(nums):
     # return nums
     
     # 3rd Approach (Optimal)
-    # n = len(nums)
-    # result = [0] * n
-    # pos_index, neg_index = 0, 1
-    # for num in nums:
-    #     if num >= 0:
-    #         result[pos_index] = num
-    #         pos_index += 2
-    #     else:
-    #         result[neg_index] = num
-    #         neg_index += 2
-    # return result
+    n = len(nums)
+    result = [0] * n
+    pos_index, neg_index = 0, 1
+    for num in nums:
+        if num >= 0:
+            result[pos_index] = num
+            pos_index += 2
+        else:
+            result[neg_index] = num
+            neg_index += 2
+    return result
 
 arr1 = [4, -2, 5, 6, -7, -1]
 arr2 = [-1, 1]
